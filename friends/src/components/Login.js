faf1809fb989d79b10edf4119165be7f48e0886d
Vincent Sanders <vincesanders@hotmail.com>
2020-01-28 14:48:46 -0600

    Added login functionality to Login including the axios post call that will return a token on a successful login and set that token to local storage.

--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,14 +1,27 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import axios from 'axios';
 
 export default () => {
+    const history = useHistory();
     const [creds, setCreds] = useState({
         username: '',
         password: ''
     });
 
+    //TODO: redux state?
+    //          --error: for error text
+    //          --isLoading: for loader
+
     const login = e => {
         e.preventDefault();
+        axios
+            .post('http://localhost:5000/api/login', creds)
+            .then(res => {
+                localStorage.setItem('token', res.data.payload);
+                history.push('/protected');
+            })
+            .catch(err => console.log(err));
     }
 
     const handleChange = e => {
